920603ec210d0655af2d8bb4c68f59a601f3de39
jojomartinez <jojomartinez@github.com>
2018-10-01 19:20:37 -0500

    updated the appletree script and made the game work so the apple will now fall

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -4,13 +4,47 @@ using UnityEngine;
 
 public class AppleTree : MonoBehaviour {
 
+	public GameObject applePrefab;
+
+	public float speed = 1f;
+
+	//Distance where Appletree turns around
+	public float leftAndRightEdge = 10f;
+	//chance that the Appletree will change directions
+	public float chanceToChangeDirections = 0.1f;
+	//rate at which our apples will be instantiated
+	public float secondsBetweenAppleDrops = 1f;
+
+
 	// Use this for initialization
 	void Start () {
-		
+		InvokeRepeating ("DropApple", 2f, secondsBetweenAppleDrops);
+	}
+
+	void DropApple()
+	{
+		GameObject apple = Instantiate(applePrefab) as GameObject;
+		apple.transform.position = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//movement
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+		//direction
+		if (pos.x < -leftAndRightEdge) {
+			speed = Mathf.Abs (speed); //move right
+		} else if (pos.x > leftAndRightEdge) {
+			speed = -Mathf.Abs (speed); //move left
+		}
+	}
+	void FixedUpdate()
+	{
+		//change direction random
+	 	if (Random.value < chanceToChangeDirections) {
+			speed *= -1; 
+		}	
 	}
 }
